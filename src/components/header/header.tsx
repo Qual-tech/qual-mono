0778247d1a0459466cb88f5e971f39e5760ea5b1
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-06-23 15:52:22 +0700

    feat: :sparkles: menu to signOut

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,12 @@
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
   const { data, status } = useSession();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className="sticky top-0 ml-16 mr-2 flex items-center justify-between bg-[#FBFAF2] p-2 px-5">
       <Link href="/">
@@ -20,17 +23,37 @@ const Header = () => {
           </li>
 
           <li>
-            {status == "authenticated" && (
-              <div className="relative h-10 w-10 overflow-hidden rounded-full object-cover object-center">
-                <Image
-                  className="object-cover object-center"
-                  fill
-                  src={
-                    data.user.image ??
-                    "https://safebooru.org//samples/4185/sample_a1789a1c4212c0de8d01217b7b0f87c4e7ce227c.jpg"
-                  }
-                  alt="profile"
-                />
+            {status == "authenticated" ? (
+              <div className="relative">
+                <button
+                  type="button"
+                  onClick={() => setIsMenuOpen(true)}
+                  className="relative h-10 w-10 overflow-hidden rounded-full object-cover object-center"
+                >
+                  <Image
+                    className="object-cover object-center"
+                    fill
+                    src={
+                      data.user.image ??
+                      "https://safebooru.org//samples/4185/sample_a1789a1c4212c0de8d01217b7b0f87c4e7ce227c.jpg"
+                    }
+                    alt="profile"
+                  />
+                </button>
+
+                <div
+                  className="absolute right-0 top-0 flex-col gap-y-2 rounded-md bg-white p-4 shadow-xl"
+                  style={{ display: isMenuOpen ? "flex" : "none" }}
+                >
+                  <div className="ml-auto">
+                    <button onClick={() => setIsMenuOpen(false)}>x</button>
+                  </div>
+                  <button onClick={() => void signOut()}>Keluar</button>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => void signIn()}>Masuk</button>
               </div>
             )}
           </li>
--- src/pages/test/record/index.tsx
+++ src/pages/test/record/index.tsx
@@ -1,6 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { Quicksand } from "next/font/google";
 import Image from "next/image";
